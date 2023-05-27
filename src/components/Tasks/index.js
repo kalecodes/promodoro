28c469470925c6a2cc87df986391e58d5f68981f
kalen510 <kalen.wiley@gmail.com>
2023-05-27 17:01:20 -0400

    tasks layout and buttons

--- src/components/Tasks/index.js
+++ src/components/Tasks/index.js
@@ -5,7 +5,19 @@ function Tasks()  {
     <section className="w-1/2 flex flex-col">
       <div className="text-2xl">Tasks</div>
       <div className="w-9/10 h-9/10 m-auto">
-        <div className="">
+        <div className="flex justify-between items-center h-1/10">
+          <img 
+            src={require('../../assets/icons/plus-dark.png')}
+            alt="add new task"
+            className="h-3/5 ml-5"
+          />
+          <img 
+            src={require('../../assets/icons/clear-dark.png')} 
+            alt="clear all tasks"
+            className="h-3/5 mr-5"
+          />
+        </div>
+        <div id="task-container" className="h-9/10 overflow-scroll">
 
         </div>
       </div>
