9f2aeef24021e238d75cdc9ee4229fd1c67657ac
kalen510 <kalen.wiley@gmail.com>
2023-05-28 13:01:18 -0400

    timer component and layout

--- src/components/Tasks/index.js
+++ src/components/Tasks/index.js
@@ -5,16 +5,16 @@ function Tasks()  {
     <section className="w-1/2 flex flex-col">
       <div className="text-2xl">Tasks</div>
       <div className="w-9/10 h-9/10 m-auto">
-        <div className="flex justify-between items-center h-1/10">
+        <div className="flex justify-between items-center h-1/10 w-4/5 mx-auto">
           <img 
             src={require('../../assets/icons/plus-dark.png')}
             alt="add new task"
-            className="h-3/5 ml-5"
+            className="h-3/5 "
           />
           <img 
             src={require('../../assets/icons/clear-dark.png')} 
             alt="clear all tasks"
-            className="h-3/5 mr-5"
+            className="h-3/5 "
           />
         </div>
         <div id="task-container" className="h-4/5 overflow-scroll">
--- src/components/Timer/index.js
+++ src/components/Timer/index.js
@@ -5,8 +5,23 @@ function Timer() {
     <div className="h-1/2 flex flex-col">
       <div className="text-2xl">Timer</div>
       <div className="w-9/10 h-4/5 m-auto">
-        <div className="">
-
+        <div>
+          <button className="text-xl py-2 px-3 bg-gray-300 border-2 border-black">25</button>
+          <button className="text-xl py-2 px-3 bg-gray-300 border-2 border-black">35</button>
+          <button className="text-xl py-2 px-3 bg-gray-300 border-2 border-black">45</button>
+          <button className="text-xl py-2 px-3 bg-gray-300 border-2 border-black">+</button>
+        </div>
+        <div className="h-3/4 flex flex-col justify-evenly items-center">
+          <img 
+            src={require('../../assets/icons/start-dark.png')}
+            alt="start timer"
+            className=""
+          />
+          <div className="text-4xl">25:00:00</div>
+        </div>
+        <div className="flex justify-evenly">
+          <div className="text-xl">Completed: 0</div>
+          <div className="text-xl">Break in: 4</div>
         </div>
       </div>
     </div>
