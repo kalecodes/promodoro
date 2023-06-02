f414acecefc09d98e1ada603a6074fe57ee874ab
kalen510 <kalen.wiley@gmail.com>
2023-06-02 17:53:04 -0400

    notes section functionality

--- src/components/Notes/index.js
+++ src/components/Notes/index.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Notes() {
+  const [notes, setNotes] = useState("");
+
+  useEffect(() => {
+    const savedNotes = localStorage.getItem("notes");
+    if (savedNotes) {
+      setNotes(savedNotes);
+    }
+  }, [])
+
+  const handleChange = e => {
+    const notes = e.target.value;
+    setNotes(notes);
+    localStorage.setItem("notes", notes);
+  }
+
   return (
     <div className="h-1/2 lg:flex flex-col hidden lg:display">
       <div className="text-2xl">Notes</div>
       <div className="w-9/10 h-4/5 m-auto">
-        <textarea placeholder="Keep your notes here..." className="w-full h-full p-2">
+        <textarea placeholder="Keep your notes here..." className="w-full h-full p-2" value={notes} onChange={handleChange}>
 
         </textarea>
       </div>
