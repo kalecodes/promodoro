6b548bca0dc0d8dc4ab759c9e40c70e55484498c
kalen510 <kalen.wiley@gmail.com>
2023-06-07 18:05:30 -0400

    re-implement task component

--- src/components/Tasks/index.js
+++ src/components/Tasks/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-// import Task from "../Task";
+import Task from "../Task";
 import TaskModal from "../TaskModal";
 
 function Tasks()  {
@@ -42,38 +42,7 @@ function Tasks()  {
           />
         </div>
         <div id="task-container" className="h-5/6 overflow-scroll order-3 lg:order-none">
-          {tasks && tasks.map((task, i) => (
-            <div key={i} className="w-9/10 h-12 my-3 mx-auto rounded bg-gray-300 flex justify-between items-center">
-              <p className="text-left ml-5">{task.title}</p>
-              <img 
-                src={require('../../assets/icons/menu-dark.png')}
-                alt="task options"
-                className="mr-5 w-7"
-              />
-              <div className="mr-5 h-full items-center hidden">
-                <img 
-                  src={require('../../assets/icons/sub-dark.png')} 
-                  alt="add sub-task"
-                  className="w-8 ml-1"
-                />
-                <img 
-                  src={require('../../assets/icons/check-dark.png')} 
-                  alt="mark task complete"
-                  className="w-7 ml-1"
-                />
-                <img 
-                  src={require('../../assets/icons/delete-dark.png')} 
-                  alt="add sub-task"
-                  className="w-7 ml-1"
-                />
-                <img 
-                  src={require('../../assets/icons/close-dark.png')} 
-                  alt="close menu"
-                  className="w-7 ml-1"
-                />
-              </div>
-            </div>
-          ))}
+          {tasks && tasks.map((task) => <Task task={task} key={task.title}/>)}
         </div>
         <div className="h-1/10 flex justify-center items-center order-2 lg:order-none">
           <div className="w-1/3 min-w-fit h-7 rounded bg-gray-300">
