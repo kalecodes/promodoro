fc69b5ac55d9e873bfcfeaf135b678c45edfa351
kalen510 <kalen.wiley@gmail.com>
2023-06-08 20:54:19 -0400

    start task menu func

--- src/components/Task/index.js
+++ src/components/Task/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
 function Task({ task })  {
+  const toggleMenu = (e) => {
+    e.target.className = "hidden"
+  }
+
   return (
     <div key={task.title} className="w-9/10 h-12 my-3 mx-auto rounded bg-gray-300 flex justify-between items-center">
       <p className="text-left ml-5">{task.title}</p>
@@ -8,27 +12,33 @@ function Task({ task })  {
         src={require('../../assets/icons/menu-dark.png')}
         alt="task options"
         className="mr-5 w-7"
+        data-id="menu"
+        onClick={toggleMenu}
       />
-      <div className="mr-5 h-full items-center hidden">
+      <div className="mr-5 h-full items-center hidden" data-id="menu-div">
         <img 
           src={require('../../assets/icons/sub-dark.png')} 
           alt="add sub-task"
           className="w-8 ml-1"
+          data-id="subtask"
         />
         <img 
           src={require('../../assets/icons/check-dark.png')} 
           alt="mark task complete"
           className="w-7 ml-1"
+          data-id="complete"
         />
         <img 
           src={require('../../assets/icons/delete-dark.png')} 
-          alt="add sub-task"
+          alt="delete task"
           className="w-7 ml-1"
+          data-id="delete"
         />
         <img 
           src={require('../../assets/icons/close-dark.png')} 
           alt="close menu"
           className="w-7 ml-1"
+          data-id="close"
         />
       </div>
     </div>
