30279c87789d53775b9167100d9940db9811d0d6
kalen510 <kalen.wiley@gmail.com>
2023-06-16 12:38:12 -0400

    task tooltips

--- src/components/Task/index.js
+++ src/components/Task/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Tooltip } from 'react-tooltip';
 import SubTask from '../SubTask';
 
 function Task({ task, triggerSubtaskModal, deleteTask, deleteSubtask })  {
@@ -22,32 +23,47 @@ function Task({ task, triggerSubtaskModal, deleteTask, deleteSubtask })  {
           alt="task options"
           className={taskMenu ? "mr-5 w-7 hidden" : "mr-5 w-7"}
           onClick={toggleMenu}
+          data-tooltip-id="task-menu-tt"
+          data-tooltip-content="Task Menu"
         />
+        <Tooltip id="task-menu-tt" />
         <div className={taskMenu ? "mr-5 h-full flex items-center" : "mr-5 h-full items-center hidden"}>
           <img 
             src={require('../../assets/icons/sub-dark.png')} 
             alt="add sub-task"
             className="w-8 ml-1"
             onClick={triggerSubtaskModal}
+            data-tooltip-id="add-sub-tt"
+            data-tooltip-content="Add a Subtask"
           />
+          <Tooltip id="add-sub-tt" />
           <img 
             src={require('../../assets/icons/check-dark.png')} 
             alt="mark task complete"
             className="w-7 ml-1"
             onClick={completeTask}
+            data-tooltip-id="complete-task-tt"
+            data-tooltip-content="Mark Task as Complete"
           />
+          <Tooltip id="complete-task-tt" />
           <img 
             src={require('../../assets/icons/delete-dark.png')} 
             alt="delete task"
             className="w-7 ml-1"
             onClick={deleteTask}
+            data-tooltip-id="delete-task-tt"
+            data-tooltip-content="Delete Task"
           />
+          <Tooltip id="delete-task-tt" />
           <img 
             src={require('../../assets/icons/close-dark.png')} 
             alt="close menu"
             className="w-7 ml-1"
             onClick={toggleMenu}
+            data-tooltip-id="close-menu-tt"
+            data-tooltip-content="Close Menu"
           />
+          <Tooltip id="close-menu-tt" />
         </div>
       </div>
       {task.subTasks && task.subTasks.map((subtask) => <SubTask subtask={subtask} deleteSubtask={deleteSubtask} key={subtask}/>)}
